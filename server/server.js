3ab711d7665436cccafd53feb2f85621f2e8dcc7
aniongithub <ani@anionline.me>
2023-01-02 06:38:08 +0000

    Can build and/or freely switch between native and WASM builds/launches now within the same source and build tree! YAY!

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ express.static.mime.define({'text/javascript': ['js']});
 const app = express();
 const httpServer = http.createServer(app);
 app.use('/', express.static('client'));
-app.use('/js/node-editor-wasm.js', express.static('build/wasm/node-editor-wasm.js'));
-app.use('/js/node-editor-wasm.js.mem', express.static('build/wasm/node-editor-wasm.js.mem'));
+app.use('/js/node-editor-wasm.js', express.static('build/node-editor-wasm-prefix/src/node-editor-wasm-build/node-editor-wasm.js'));
+app.use('/js/node-editor-wasm.js.mem', express.static('build/node-editor-wasm-prefix/src/node-editor-wasm-build/node-editor-wasm.js.mem'));
 
 const port = process.env.PORT || 80;
 httpServer.listen(port, () => console.log(`Listening on port ${port}!`));
